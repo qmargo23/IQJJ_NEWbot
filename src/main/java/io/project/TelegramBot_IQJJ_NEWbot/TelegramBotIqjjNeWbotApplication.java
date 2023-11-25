05381ed09067bb90aa4910a097dd673f3d0d650c
qmargo23 <elen.asplund@gmail.com>
2023-11-25 21:05:17 +0300

    init bot (/start)

---
+++ src/main/java/io/project/TelegramBot_IQJJ_NEWbot/TelegramBotIqjjNeWbotApplication.java
@@ -0,0 +1,13 @@
+package io.project.TelegramBot_IQJJ_NEWbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotIqjjNeWbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotIqjjNeWbotApplication.class, args);
+	}
+
+}
